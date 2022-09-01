c701d4d2573dd98848f85086e2b4c4c298d46da3
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-08-31 20:20:52 -0600

    wrote first test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,18 @@
 const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  it("should create an employee object with keys of name, id, and email", () => {
+    const testEmployee = new Employee("testName", "testID", "testEmail");
+    expect(testEmployee.name).toEqual("testName");
+    expect(testEmployee.id).toEqual("testID");
+    expect(testEmployee.email).toEqual("testEmail");
+  });
+
+  it("should create an employee object with methods of getName(), getID(), getEmail(), and getRole() that return the correct values", () => {
+    const testEmployee = new Employee("testName", "testID", "testEmail");
+    expect(testEmployee.getName()).toEqual("testName");
+    expect(testEmployee.getId()).toEqual("testID");
+    expect(testEmployee.getEmail()).toEqual("testEmail");
+    expect(testEmployee.getRole()).toEqual("Employee");
+  });
+});
